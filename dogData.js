f1c013af7012b479d8fa5e2289fd90b21d34eb81
bst0705 <xiangtaishiqiao0@gmail.com>
2022-01-08 14:51:06 +0900

    package2

--- dogData.js
+++ dogData.js
@@ -1,4 +1,10 @@
+import readlineSync from "readline-sync";
 import Dog from "./dog.js";
-  const dog1 = new Dog("レオ", 4, "チワワ");
-  const dog2 = new Dog("太郎", 2, "柴犬");
-export {dog1,dog2};
+
+const dog1 = new Dog("レオ", 4, "チワワ");
+const name = readlineSync.question("名前を入力してください: ");
+const age = readlineSync.questionInt("年齢を入力してください: ");
+const breed = readlineSync.question("犬種を入力してください: ");
+const dog2 = new Dog(name, age, breed);
+
+export { dog1, dog2 };
