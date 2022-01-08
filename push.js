9a606a58bc6719777773b3ba6957d2a5c0e946fb
bst0705 <xiangtaishiqiao0@gmail.com>
2022-01-08 15:50:38 +0900

    forEach

---
+++ push.js
@@ -0,0 +1,7 @@
+/* pushメソッドとは、配列の最後に新しい要素を追加するメソッドです。
+pushメソッドの後の()の中に追加したい要素を入力します。
+以下の例では、pushメソッドの引数「4」が配列の最後に追加されています。 */
+const numbers = [1,2,3];
+console.log(numbers);
+numbers.push(4);
+console.log(numbers);
